718083276debf82736d2172d3a2a34f98f3c1b62
Jonathan Blok <jonathan.blok@jdcrp.org>
2024-10-08 13:28:24 +0200

    Adding filename cleanup operations.

--- scripts/nara-resize-webfriendly-7102924.sh
+++ scripts/nara-resize-webfriendly-7102924.sh
@@ -1,10 +1,26 @@
 # /bin/sh
 
 cd ..
+
+# Find the TIFs and bulk resize
 find -iname '*.tif' -exec mogrify -resize 1092266@ -format jpg {} +
+
+# Copy JPGs and directory structure to jpg directory
 rsync -a --include '*/' --include '*.jpg' --exclude '*' tif/ jpg/
+
+# Remove JPGs from tif directory
 cd tif
 find -iname '*.jpg' -exec rm {} +
+
 cd ../jpg
-find . -type f >> ../filelist.txt
+
+# Remove whitespace and '(FINISHED)' from directories
+find . -type d -exec rename -n 's/\s//' {} \; -exec rename -n 's/\(FINISHED\)//' {} \;
+
+# Remove whitespace from filenames
+find . -depth -iname '*.jpg' -type f -exec rename -n 's/\s//' {} +
+
+# Export dirlist/filelist as csv
+find . -type f -printf '"%p",\n' > ../filelist.csv
+
 cd ../scripts
