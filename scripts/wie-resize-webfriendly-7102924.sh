e7cb5970356e3899a596150b1124918f6359a97d
Jonathan Blok <jonathan.blok@jdcrp.org>
2024-10-09 15:11:53 +0200

    Adding WIE resizer.

--- scripts/wie-resize-webfriendly-7102924.sh
+++ scripts/wie-resize-webfriendly-7102924.sh
@@ -4,10 +4,11 @@ echo 'Starting preprocessing.. '
 cd ..
 rm -r jpg/*
 
+
 # Find the TIFs and bulk resize
 echo 'Resizing and reformatting TIFs as JPGs.. '
 
-find -iname '*.tif' -exec mogrify -resize 1092266@ -format jpg {} \; -exec echo {}': Done' \;
+find -iname '*.jpg' -exec mogrify -resize 1092266@ -format jpg {} \; -exec echo {}': Done' \;
 
 # Move JPGs and directory structure to jpg directory
 echo 'Copying files to jpg directory.. '
@@ -23,7 +24,7 @@ cd ../jpg
 # Remove whitespace and '(FINISHED)' from directories
 echo 'Cleaning directory and file names.. '
 
-find -type d -exec rename 's/\s//' * {} +
+find -type d -exec rename 's/\s/_/' * {} +
 find -type d -exec rename 's/\(FINISHED\)//' {} +
 find -type d -exec rename 's/#//' {} +
 find -type d -exec rename 's/Roll/Roll_/' {} +
